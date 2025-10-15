080e32e50337b5f8da2988689c696e1e86d5702b
kietnx <kietnx@rabiloo.com>
2025-10-15 11:30:54 +0700

    update: base jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  // cho phép transform các lib RN trong node_modules (không bỏ qua)
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-native-gesture-handler' +
+      '|react-native-reanimated' +
+      '|@react-navigation' +
+      ')/)',
+  ],
+  setupFiles: ['<rootDir>/jest.setup.js'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'react-native-gesture-handler/jestSetup',
+  ],
+  testPathIgnorePatterns: ['/node_modules/', '/android/', '/ios/'],
+  moduleNameMapper: {
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '\\.(png|jpg|ico|jpeg|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+  },
 };
---
+++ jest.setup.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+import 'react-native-gesture-handler/jestSetup';
+global.__reanimatedWorkletInit = () => {};
+
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
